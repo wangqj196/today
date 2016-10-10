4cba30128a7699b34a0e8026631927a49234d4ac
wangqj <3001162649@qq.com>
2016-10-10 17:14:21 +0800

    Z

--- src/com/to/today/BB.java
+++ src/com/to/today/BB.java
@@ -2,6 +2,10 @@ package com.to.today;
 
 public class BB {
 	public void testb() {
+
+		System.out.println("111");
+
+		System.out.println("啊啊啊");
 		System.out.println("啊啊啊");
 		System.out.println("aaa发反反复复");
 		System.out.println("aaahhhhhh");
