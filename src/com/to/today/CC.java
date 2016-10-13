200028277bb79b8c0a6ca42577d49ef4006102a3
wangqj <3001162649@qq.com>
2016-10-13 10:02:26 +0800

    11

---
+++ src/com/to/today/CC.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package com.to.today;
+
+/**
+ * @Description: 这里用一句话描述这个类的作用
+ * @see: CC 此处填写需要参考的类
+ * @version 2016年10月13日 上午9:53:40
+ * @author wangqj
+ */
+public class CC {
+	public void testcc() {
+		System.out.println("000");
+		System.out.println("111");
+		System.out.println("222");
+		System.out.println("啊啊啊");
+		System.out.println("啊啊啊");
+		System.out.println("aaa发反反复复");
+		System.out.println("aaahhhhhh");
+		System.out.println("asdasd");
+	}
+}
---
+++ today/src/com/to/today/AA.java
@@ -0,0 +1,72 @@
+/**
+ * 
+ */
+package com.to.today;
+
+/**
+ * @Description: 这里用一句话描述这个类的作用
+ * @see: AA 此处填写需要参考的类
+ * @version 2016年10月9日 下午5:51:22
+ * @author wangqj
+ */
+public class AA {
+
+	public void aa() {
+
+		System.out.println("hhhhhhhhh");
+
+		System.out.println("ddddddddddddd");
+		System.out.println("fffffffffffff");
+
+		System.out.println("哈哈哈哈哈哈阿萨德哈as");
+		System.out.println("fffffffffffff");
+
+		System.out.println("哈哈哈哈哈哈阿萨德哈as");
+		System.out.println("哈哈哈哈哈哈阿萨德哈as");
+
+		System.out.println("哈哈哈哈哈哈哈eeeeeeeeeeeee");
+
+		System.out.println("4444刚刚灌灌灌灌灌灌灌灌灌灌4");
+		System.out.println("弟弟顶顶顶顶顶顶顶顶顶顶");
+		System.out.println("啊啊啊");
+		System.out.println("啊啊啊");
+		System.out.println("啊啊啊");
+		System.out.println("啊啊啊");
+		System.out.println("啊啊啊");
+		System.out.println("aaa发反反复复");
+		System.out.println("aaahhhhhh");
+		System.out.println("asdasd");
+	}
+
+	public void aab() {
+		System.out.println("啊啊啊");
+		System.out.println("aaa发反反复复");
+		System.out.println("aaahhhhhh");
+		System.out.println("asdasd");
+	}
+
+	public void ff() {
+		System.out.println("asdasd");
+		System.out.println("asdasd");
+		System.out.println("asdasd");
+	}
+
+	public void mm() {
+		System.out.println("asdasd");
+		System.out.println("asdasd");
+		System.out.println("asdasd");
+	}
+
+	public void mum() {
+		System.out.println("asdasd");
+		System.out.println("asdasd");
+		System.out.println("asdasd");
+	}
+
+	public void num() {
+		System.out.println("asdasd");
+		System.out.println("asdasd");
+		System.out.println("asdasd");
+	}
+
+}
