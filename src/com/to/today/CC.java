789d86a3d74fb804f47be59210b14c4f0801b94b
wangqj <3001162649@qq.com>
2016-10-13 10:11:02 +0800

    AC

--- src/com/to/today/CC.java
+++ src/com/to/today/CC.java
@@ -20,4 +20,15 @@ public class CC {
 		System.out.println("aaahhhhhh");
 		System.out.println("asdasd");
 	}
+
+	public void testAc() {
+		System.out.println("000");
+		System.out.println("111");
+		System.out.println("222");
+		System.out.println("啊啊啊");
+		System.out.println("啊啊啊");
+		System.out.println("aaa发反反复复");
+		System.out.println("aaahhhhhh");
+		System.out.println("asdasd");
+	}
 }
